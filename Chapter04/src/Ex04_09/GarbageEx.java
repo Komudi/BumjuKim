31b6d1aeb110ade0ba3462f1b224824488da670e
bobkim <hot4323@naver.com>
2020-05-05 18:32:07 +0900

    Chapter04 11/11

---
+++ Chapter04/src/Ex04_09/GarbageEx.java
@@ -0,0 +1,17 @@
+package Ex04_09;
+
+public class GarbageEx {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String a= new String("Good");
+		String b= new String("Bad");
+		String c= new String("Normal");
+		String d,e;
+		
+		a= null;
+		d= c;
+		c= null;
+	}
+
+}
