f6e173c9c68fac36d1c2ba01f5fa3c962c77fddf
bobkim <hot4323@naver.com>
2020-05-05 18:17:28 +0900

    Chapter04 7/11

---
+++ Chapter04/src/Ex04_04/Book.java
@@ -0,0 +1,27 @@
+package Ex04_04;
+
+public class Book {
+	String title;
+	String author;
+	public Book(String t) {
+		title = t;
+		author = "작자미상";
+	}
+	
+	public Book(String t, String a) {
+		title =t;
+		author = a;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Book littlePrince = new Book("어린왕자", "생텍");
+		
+		Book loveStory = new Book("춘향이");
+		
+		System.out.println(littlePrince.title +" "+ littlePrince.author);
+		System.out.println(loveStory.title+ " " +loveStory.author);
+
+	}
+
+}
---
+++ Chapter04/src/Ex04_05/Book.java
@@ -0,0 +1,10 @@
+package Ex04_05;
+
+public class Book {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
