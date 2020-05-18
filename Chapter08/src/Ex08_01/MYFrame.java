04d915e372b95f48d47f75de06ff8c107da5196b
bobkim <hot4323@naver.com>
2020-05-18 20:38:47 +0900

    CH08 COMPL 6/6

---
+++ Chapter08/src/Ex08_01/MYFrame.java
@@ -0,0 +1,17 @@
+package Ex08_01;
+
+import javax.swing.JFrame;
+
+public class MYFrame extends JFrame{
+	public MYFrame() {
+		setTitle("300x300 스윙 프레임 만들기");
+		setSize(300,300);
+		setVisible(true);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MYFrame frame = new MYFrame();
+	}
+
+}
